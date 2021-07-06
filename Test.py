9c668e67e978f17e85396eeb06cb53d5fdb3464a
zero_0_1103 <83809838+dlwlsdudo1@users.noreply.github.com>
2021-07-06 23:32:29 +0900

    개인리뷰 할 것
    ㅇ

---
+++ Test.py
@@ -0,0 +1,74 @@
+# 예제 3-38 라이브러리 추가하기
+from selenium import webdriver 
+from bs4 import BeautifulSoup 
+import time
+import pandas as pd
+# 예제 3-39 webdriver로 크롬 브라우저 실행하기
+browser = webdriver.Chrome('c:/drive/chromedriver.exe')
+url = "https://youtube-rank.com/board/bbs/board.php?bo_table=youtube" 
+browser.get(url)
+# 예제 3-40 페이지 정보 가져오기
+html = browser.page_source
+soup = BeautifulSoup(html, 'html.parser')
+# 예제 3-41 BeautifulSoup으로 tr 태그 추출하기
+channel_list = soup.select('tr') 
+print(len(channel_list), '\n')
+print(channel_list[0])
+# 예제 3-42 tr 태그 확인하기
+channel_list = soup.select('form > table > tbody > tr')
+print(len(channel_list))
+# 예제3-43채널태그출력및태그구조 확인하기
+channel = channel_list[0]
+print(channel)
+# 예제 3-44 카테고리 정보 추출하기
+category = channel.select('p.category')[0].text.strip()
+print(category)
+# 예제 3-45 채널명 찾아오기
+title = channel.select('h1 > a')[0].text.strip()
+print(title)
+# 예제 3-46 구독자 수, View 수, 동영상 수 추출하기
+subscriber = channel.select('.subscriber_cnt')[0].text
+view = channel.select('.view_cnt')[0].text
+video = channel.select('.video_cnt')[0].text
+print(subscriber)
+print(view)
+print(video)
+
+
+
+# 예제 3-47 반복문으로 채널 정보 추출하기
+channel_list = soup.select('tbody > tr') 
+for channel in channel_list:
+    title = channel.select('h1 > a')[0].text.strip()
+    category = channel.select('p.category')[0].text.strip()
+    subscriber = channel.select('.subscriber_cnt')[0].text
+    view = channel.select('.view_cnt')[0].text
+    video = channel.select('.video_cnt')[0].text 
+    print(title, category, subscriber, view, video)
+    # 예제 3-48 페이지별 URL 만들기
+page = 1
+url = 'https://youtube-rank.com/board/bbs/board.php?bo_table=youtube&page={}'.format(page)
+print(url)
+# 예제 3-49 반복문으로 유튜브 랭킹 화면의 여러 페이지를 크롤링하기
+results = []
+for page in range(1,11):
+    url = f"https://youtube-rank.com/board/bbs/board.php?bo_table=youtube&page={page}" 
+    browser.get(url)
+    time.sleep(2)
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    channel_list = soup.select('form > table > tbody > tr')
+    for channel in channel_list:
+        title = channel.select('h1 > a')[0].text.strip() 
+        category = channel.select('p.category')[0].text.strip()
+        subscriber = channel.select('.subscriber_cnt')[0].text 
+        view = channel.select('.view_cnt')[0].text
+        video = channel.select('.video_cnt')[0].text
+        data = [title, category, subscriber, view, video]
+        results.append(data)
+        browser.quit()
+        # 예제 3-50 데이터 칼럼명을 설정하고 엑셀 파일로 저장하기
+df = pd.DataFrame(results)
+df.columns = ['title', 'category', 'subscriber', 'view', 'video']
+df.to_excel('youtube_rank.xlsx', index = False)
+print("=========================")
