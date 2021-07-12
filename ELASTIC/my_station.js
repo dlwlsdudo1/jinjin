22f9b79b87ca0901ce8aa15fb4a2b9ea315252fd
zero_0_1103 <83809838+dlwlsdudo1@users.noreply.github.com>
2021-07-12 23:39:06 +0900

    elastic
    g

---
+++ ELASTIC/my_station.js
@@ -0,0 +1,21 @@
+PUT my_stations/_bulk
+{"index": {"_id": "1"}}
+{"date": "2020-06-01", "line": "1호선", "station": "종각", "passangers": 2314}
+{"index": {"_id": "2"}}
+{"date": "2020-06-01", "line": "2호선", "station": "강남", "passangers": 5412}
+{"index": {"_id": "3"}}
+{"date": "2020-07-10", "line": "2호선", "station": "강남", "passangers": 6221}
+{"index": {"_id": "4"}}
+{"date": "2020-07-15", "line": "2호선", "station": "강남", "passangers": 6478}
+{"index": {"_id": "5"}}
+{"date": "2020-08-07", "line": "2호선", "station": "강남", "passangers": 5821}
+{"index": {"_id": "6"}}
+{"date": "2020-08-18", "line": "2호선", "station": "강남", "passangers": 5724}
+{"index": {"_id": "7"}}
+{"date": "2020-09-02", "line": "2호선", "station": "신촌", "passangers": 3912}
+{"index": {"_id": "8"}}
+{"date": "2020-09-11", "line": "3호선", "station": "양재", "passangers": 4121}
+{"index": {"_id": "9"}}
+{"date": "2020-09-20", "line": "3호선", "station": "홍제", "passangers": 1021}
+{"index": {"_id": "10"}}
+{"date": "2020-10-01", "line": "3호선", "station": "불광", "passangers": 971}
